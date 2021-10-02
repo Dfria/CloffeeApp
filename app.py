c83e065eb20888a2a8f9ba4e07b228c2a822c642
Dfria <jdfria@gmail.com>
2021-10-02 00:04:49 -0700

    First commit

--- app.py
+++ app.py
@@ -3,11 +3,11 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template("form.html")
 
